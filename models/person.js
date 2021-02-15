9bbf0e3650c47e7afaa64b720e0bee04adfbcf94
dmcarmen <dmcarmen1999@gmail.com>
2021-02-15 23:30:15 +0200

    uibuild

--- models/person.js
+++ models/person.js
@@ -13,8 +13,8 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFind
   })
 
 const personSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  number: { type: String, required: true, unique: true },
+  name: { type: String, minlength: 3, required: true, unique: true },
+  number: { type: String, minlength: 8, required: true, unique: true },
 })
 
 personSchema.set('toJSON', {
