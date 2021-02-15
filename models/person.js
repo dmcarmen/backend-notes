81e80c30124eeadde9282272b11879dc853ccd20
dmcarmen <dmcarmen1999@gmail.com>
2021-02-15 23:46:05 +0200

    uibuild

--- models/person.js
+++ models/person.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
 
 const url = process.env.MONGODB_URI
+
 console.log('connecting to', url)
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
